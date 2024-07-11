8772e2e69862e5a5d6700d75612bcd4e4837bc70
klansman <akachukwuvictor29@gmail.com>
2024-07-11 10:01:28 +0100

    dynamic content

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index'),
-    path('counter', views.counter, name='counter'),
+    path('', views.index, name='index')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Services
 
 # Create your views here.
 def index(request):
-    context = {
-        'name':'Junior',
-        'age':'30',
-        'nationality':'American'
-    }
-    return render(request, 'index.html', context) 
+    service1 = Services()
+    service1.id = 0
+    service1.name = 'Fast'
+    service1.description = 'Our Services is very fast'
 
-def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    service2 = Services()
+    service2.id = 1
+    service2.name = 'Reliable'
+    service2.description = 'Our Services is very Reliable'
+
+    service3 = Services()
+    service3.id = 2
+    service3.name = 'Affordable'
+    service3.description = 'Our Services is very Cheap'
+
+    service4 = Services()
+    service4.id = 3
+    service4.name = 'Easy to Use'
+    service4.description = 'Our Services is Easy to Use'
+
+    service = [service1, service2, service3, service4]
+    return render(request, 'index.html', {'services': service})
