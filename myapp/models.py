802e7a3019b61a2320253e036b741b1e14a69170
klansman <akachukwuvictor29@gmail.com>
2024-07-12 14:34:11 +0100

    created login pages for admin and user Authentication

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Services:
-    id: int
-    name: str
-    description: str
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
--- myapp/views.py
+++ myapp/views.py
@@ -1,28 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Services
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    service1 = Services()
-    service1.id = 0
-    service1.name = 'Fast'
-    service1.description = 'Our Services is very fast'
-
-    service2 = Services()
-    service2.id = 1
-    service2.name = 'Reliable'
-    service2.description = 'Our Services is very Reliable'
-
-    service3 = Services()
-    service3.id = 2
-    service3.name = 'Affordable'
-    service3.description = 'Our Services is very Cheap'
-
-    service4 = Services()
-    service4.id = 3
-    service4.name = 'Easy to Use'
-    service4.description = 'Our Services is Easy to Use'
-
-    service = [service1, service2, service3, service4]
+    service = Services.objects.all()
     return render(request, 'index.html', {'services': service})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already registered')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already registered')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+    else:        
+        return render(request, 'register.html')
