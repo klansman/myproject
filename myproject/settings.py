25aa71f7f0a4562543017d3bb83c640872779115
klansman <akachukwuvictor29@gmail.com>
2024-07-16 08:01:38 +0100

    moved from dlsql to postgresql

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'Cyberghost',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
