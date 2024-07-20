86885184e9d1ca2af24a386279cf4c378ce93401
klansman <akachukwuvictor29@gmail.com>
2024-07-20 12:15:37 +0100

    modification to fix error in getting jason data for today

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -6,6 +6,9 @@ import urllib.request, json
 
 # Create your views here.
 def index(request):
+    #initializing the city and data variables to empty strings and an empty dictionary respectively
+    city = ''
+    data = {}   
     #get the inputed city from the html file
     if request.method =='POST':
         city = request.POST['city']
@@ -13,14 +16,16 @@ def index(request):
         res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/forecast?q='+city+'&APPID=cf646e05484370594fd5227c09d31542').read()
         #load the weather data from res to var jason_data whch saves the weather data as a json file
         json_data = json.loads(res)
+        #get the weather data for today (0 index) from the list of forecasts and save it in f
+        f = json_data['list'][0]
         #Now we have to convert the weather data to a dictionary so that django can read it
-        data={
-           "country_code":str(json_data['sys']['country']),
-           "coordinates":str(json_data['coord']['lon']+''+['coord']['lat']),
-           "temp":str(json_data['main']['temp']),
-           "pressure":str(json_data['main']['pressure']),
-           "humidity":str(json_data['main']['humidity']),
+        data = {
+           "country_code":str(json_data['city']['country']),
+           "coordinates":str(json_data['city']['coord']),
+           "temp":str(f['main']['temp']),
+           "pressure":str(f['main']['pressure']),
+           "humidity":str(f['main']['humidity'])
         }
     else:
-     data={}
-    return render(request, 'index.html', {'data':data}, {'city':city})
+        data={}
+    return render(request, 'index.html', {'data':data, 'city':city})
